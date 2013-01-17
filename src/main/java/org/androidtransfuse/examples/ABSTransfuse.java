b6ef9225def9c9a1fe4b826c16158c459aa2efa0
John Ericksen <johncarl81@gmail.com>
2013-01-16 23:53:18 -0700

    Added theme for proper back-version support

--- src/main/java/org/androidtransfuse/examples/ABSTransfuse.java
+++ src/main/java/org/androidtransfuse/examples/ABSTransfuse.java
@@ -6,7 +6,7 @@ import org.androidtransfuse.annotations.*;
 /**
  * @author John Ericksen
  */
-@Activity(type = SherlockActivity.class)
+@Activity(type = SherlockActivity.class, theme = "@style/Theme.Sherlock")
 @Layout(R.layout.text)
 @IntentFilter({
         @Intent(type = IntentType.ACTION, name = android.content.Intent.ACTION_MAIN),
