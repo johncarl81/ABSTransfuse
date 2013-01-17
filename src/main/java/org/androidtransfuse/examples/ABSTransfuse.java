e15ea848298b3469e826f605701d029b6f101b10
John Ericksen <johncarl81@gmail.com>
2013-01-16 21:12:33 -0700

    Initial project structure

---
+++ src/main/java/org/androidtransfuse/examples/ABSTransfuse.java
@@ -0,0 +1,18 @@
+package org.androidtransfuse.examples;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import org.androidtransfuse.annotations.*;
+
+/**
+ * @author John Ericksen
+ */
+@Activity(type = SherlockActivity.class)
+@Layout(R.layout.text)
+@IntentFilter({
+        @Intent(type = IntentType.ACTION, name = android.content.Intent.ACTION_MAIN),
+        @Intent(type = IntentType.CATEGORY, name = android.content.Intent.CATEGORY_LAUNCHER)
+})
+public class ABSTransfuse {
+
+
+}
---
+++ src/main/java/org/androidtransfuse/examples/ABSTransfuseApplication.java
@@ -0,0 +1,10 @@
+package org.androidtransfuse.examples;
+
+import org.androidtransfuse.annotations.Application;
+
+/**
+ * @author John Ericksen
+ */
+@Application(label = "@string/application_name", icon = "@drawable/icon")
+public class ABSTransfuseApplication {
+}
